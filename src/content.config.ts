8ed68ea2c51823cc4a53d5ef42a1fdc7523dc579
Matt Seccafien <mseccafien@gmail.com>
2025-02-26 22:39:21 +0100

    Format files with prettier

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,33 +1,41 @@
-import { glob } from 'astro/loaders';
-import { defineCollection, z } from 'astro:content';
+import {glob} from 'astro/loaders'
+import {defineCollection, z} from 'astro:content'
 
 export const collections = {
-	work: defineCollection({
-		// Load Markdown files in the src/content/work directory.
-		loader: glob({ base: './src/content/work', pattern: '**/*.md', }),
-		schema: z.object({
-			title: z.string(),
-			description: z.string(),
-			publishDate: z.coerce.date(),
-			tags: z.array(z.string()),
-			img: z.string().optional(),
-			img_alt: z.string().optional(),
-			type: z.string(),
-			url: z.string().url().optional(),
-			roles: z.array(z.string()).optional(),
-			collaborators: z.array(z.object({
-				name: z.string(),
-				url: z.string().url().optional(),
-				role: z.array(z.string()).optional(),
-			})).optional(),
-			media: z.array(z.object({
-				type: z.enum(['image', 'video']),
-				src: z.string(),
-				alt: z.string(),
-			})).optional(),
-			color: z.string().optional(),
-			published: z.boolean().optional(),
-			video: z.boolean().optional(),
-		}),
-	}),
-};
+  work: defineCollection({
+    // Load Markdown files in the src/content/work directory.
+    loader: glob({base: './src/content/work', pattern: '**/*.md'}),
+    schema: z.object({
+      title: z.string(),
+      description: z.string(),
+      publishDate: z.coerce.date(),
+      tags: z.array(z.string()),
+      img: z.string().optional(),
+      img_alt: z.string().optional(),
+      type: z.string(),
+      url: z.string().url().optional(),
+      roles: z.array(z.string()).optional(),
+      collaborators: z
+        .array(
+          z.object({
+            name: z.string(),
+            url: z.string().url().optional(),
+            role: z.array(z.string()).optional(),
+          }),
+        )
+        .optional(),
+      media: z
+        .array(
+          z.object({
+            type: z.enum(['image', 'video']),
+            src: z.string(),
+            alt: z.string(),
+          }),
+        )
+        .optional(),
+      color: z.string().optional(),
+      published: z.boolean().optional(),
+      video: z.boolean().optional(),
+    }),
+  }),
+}
--- src/utils/randomNumber.ts
+++ src/utils/randomNumber.ts
@@ -1,3 +1,3 @@
 export function randomNumber(min, max, correction = 0) {
-  return Math.floor((Math.random() - correction) * (max - min + 1)) + min;
+  return Math.floor((Math.random() - correction) * (max - min + 1)) + min
 }
