b460c4c341b55c8bc2fb722c71b3a1a7fdac58e1
Matt Seccafien <mseccafien@gmail.com>
2025-03-03 23:36:25 +0100

    Fully refactor component css to globals

--- src/utils/randomNumber.ts
+++ src/utils/randomNumber.ts
@@ -1,3 +1,3 @@
-export function randomNumber(min, max, correction = 0) {
+export function randomNumber(min: number, max: number, correction = 0) {
   return Math.floor((Math.random() - correction) * (max - min + 1)) + min
 }
