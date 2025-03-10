6118ab9ba0ef8e7ba4816ef4726b7cfae9d9d537
Matt Seccafien <mseccafien@gmail.com>
2025-03-10 04:34:16 +0100

    update stylelint rules and modify typography

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,23 @@
 /** @type {import('stylelint').Config} */
 export default {
   extends: ['@efficientcss/stylelint-config-ecss'],
+  rules: {
+    'ecss/padding-constraints': null,
+    'ecss/selector-filename': [
+      true,
+      {
+        ignoreFiles: [
+          'quarantine.css',
+          'main.css',
+          'base.css',
+          'reset.css',
+          'general.css',
+          'variables.css',
+          'theme.css',
+          '/style/',
+          'typography.css',
+        ],
+      },
+    ],
+  },
 }
